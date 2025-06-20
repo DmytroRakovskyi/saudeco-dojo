b4338a5f2f45ef4e1a845818114ecdb7bde1dac6
DmytroRakovskyi <dmytriy.rakovskiy@gmail.com>
2025-06-20 18:10:19 +0200

    refactor: add test case ID to success purchase scenario for clarity

--- tests/e2e/purchase.spec.ts
+++ tests/e2e/purchase.spec.ts
@@ -8,24 +8,28 @@ import { standartUser } from '../../src/test-data/test-users';
 import { customer } from '../../src/test-data/test-customers';
 
 test.describe('purchase scenarios', { tag: ['@smoke', '@purchase'] }, () => {
-  test('success purchase', { tag: ['@smoke', '@success', '@purchase'] }, async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    const inventoryPage = new InventoryPage(page);
-    const cartPage = new CartPage(page);
-    const checkoutPage = new CheckoutPage(page);
+  test(
+    'SAU-001, success purchase',
+    { tag: ['@smoke', '@success', '@purchase'] },
+    async ({ page }) => {
+      const loginPage = new LoginPage(page);
+      const inventoryPage = new InventoryPage(page);
+      const cartPage = new CartPage(page);
+      const checkoutPage = new CheckoutPage(page);
 
-    await loginPage.goto();
-    await loginPage.userLoginIn(standartUser);
-    await inventoryPage.addItemToTheCart('backpack');
-    await inventoryPage.header.clickOnCart();
-    await expect(cartPage.item.inventoryItemName).toHaveText('Sauce Labs Backpack');
-    await expect(cartPage.item.inventoryItemPrice).toContainText('29.99');
-    await cartPage.goToCheckout();
-    await checkoutPage.fillCheckoutForm(customer);
-    await checkoutPage.continueCheckout();
-    await expect(checkoutPage.item.inventoryItemName).toHaveText('Sauce Labs Backpack');
-    await expect(checkoutPage.item.inventoryItemPrice).toContainText('29.99');
-    await checkoutPage.finishCheckout();
-    await expect(checkoutPage.checkouteCompleteHeader).toHaveText('Thank you for your order!');
-  });
+      await loginPage.goto();
+      await loginPage.userLoginIn(standartUser);
+      await inventoryPage.addItemToTheCart('backpack');
+      await inventoryPage.header.clickOnCart();
+      await expect(cartPage.item.inventoryItemName).toHaveText('Sauce Labs Backpack');
+      await expect(cartPage.item.inventoryItemPrice).toContainText('29.99');
+      await cartPage.goToCheckout();
+      await checkoutPage.fillCheckoutForm(customer);
+      await checkoutPage.continueCheckout();
+      await expect(checkoutPage.item.inventoryItemName).toHaveText('Sauce Labs Backpack');
+      await expect(checkoutPage.item.inventoryItemPrice).toContainText('29.99');
+      await checkoutPage.finishCheckout();
+      await expect(checkoutPage.checkouteCompleteHeader).toHaveText('Thank you for your order!');
+    },
+  );
 });
